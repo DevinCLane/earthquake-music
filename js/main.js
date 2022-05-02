f44e1b6514b56a3fc693f93a147fbb111a0792fa
Devin Lane <devin.c.lane@gmail.com>
2022-05-02 16:30:06 -0700

    display a map, centered on USA

--- js/main.js
+++ js/main.js
@@ -47,6 +47,23 @@ async function start() {
     }
 }
 
+// Leaflet map
+
+// initialize map
+let map = L.map('map').setView([30, -98], 4);
+
+// add the OpenStreetMap tiles
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+    }).addTo(map);
+
+// show the scale bar on the lower left corner
+L.control.scale({imperial: true, metric: true}).addTo(map);
+
+// show a marker on the map
+L.marker({lon: 0, lat: 0}).bindPopup('The center of the world').addTo(map);
+
 /* to do
 map magnitude to the font size
 map the magnitude to change volume
