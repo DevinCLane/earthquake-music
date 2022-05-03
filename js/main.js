84890a7ad68907ecfd66a03989fa4ec9fba68b1f
Devin Lane <devin.c.lane@gmail.com>
2022-05-03 12:16:10 -0700

    add stop button. simply reload the page for now

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
+// Leaflet map
+
+// initialize map
+let map = L.map('map').setView([30, -98], 4);
+
+// add the OpenStreetMap tiles
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+    }).addTo(map);
+
+// show the scale bar on the lower left corner
+L.control.scale({imperial: true, metric: true}).addTo(map);
+
+// show a marker on the map
+L.marker({lon: 0, lat: 0}).bindPopup('The center of the world').addTo(map);
+
+
+// stop everything
+function stop() {
+    window.location.reload();
+}
+
 // use an async function to minimize nesting
 async function start() {
     // request the earthquakes 
@@ -47,22 +70,6 @@ async function start() {
     }
 }
 
-// Leaflet map
-
-// initialize map
-let map = L.map('map').setView([30, -98], 4);
-
-// add the OpenStreetMap tiles
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
-    }).addTo(map);
-
-// show the scale bar on the lower left corner
-L.control.scale({imperial: true, metric: true}).addTo(map);
-
-// show a marker on the map
-L.marker({lon: 0, lat: 0}).bindPopup('The center of the world').addTo(map);
 
 /* to do
 map magnitude to the font size
