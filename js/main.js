99ffe1d78a20dc78d271bf93d3f6bf949d3c06a2
Devin Lane <devin.c.lane@gmail.com>
2022-05-04 10:28:22 -0700

    map magnitude to audio frequency

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 // Leaflet map
-
 // initialize map
 let map = L.map('map').setView([30, -98], 4);
 
@@ -25,8 +24,6 @@ async function start() {
     
     // use destructuring assignment to get the features property
     const { features } = await fetch(url).then((res) => res.json()); // parse response as JSON
-    
-
 
     // reverse the features array to get them in chronological order
     features.reverse();
@@ -34,34 +31,45 @@ async function start() {
 
     // Create a synth using tone.js and connect it to the main output (computer speakers)
     const synth = new Tone.Synth().toDestination();
-    // the notes that we will play through
-    const notes = ["C4", "D4", "E4", "G4", "A4", "C5"];
-    // set up a counter variable
-    let i = 0;
+
     // loop over the array
     for (const feature of features) {
         // calculate the time since the first earthquake
         const diffFromStart = feature.properties.time - features[0].properties.time;
-        
+        // calculate where in our timeline of one minute to play each earthquake
         feature.playtime = diffFromStart / (24 * 60 * 1000)
         console.log(feature.playtime);
 
+        // scale data function (we're converting earthquake magnitudes (~0-10) into a musical range of pitches (~40-900hz))
+        // thanks to https://writingjavascript.com/scaling-values-between-two-ranges
+        const scaleData = function scale(value, inMin, inMax, outMin, outMax) {
+            const result = (value - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+          
+            if (result < outMin) {
+              return outMin;
+            } else if (result > outMax) {
+              return outMax;
+            }
+          
+            return result;
+          }
+        // add this property to our object
+        feature.scaledTone = scaleData(feature.properties.mag, 0, 10, 40, 900)
+        console.log(feature.scaledTone)
+        
+        // create a timer for playing the notes
         setTimeout(
-            (noteValue) => {
+            () => {
                 // create geoJSON layer
                 var myLayer = L.geoJSON(feature).addTo(map);
                 // add the coordinates of the earthquake to the map
                 myLayer.addData(feature.geometry.coordinates);
                 console.log(feature);
-                console.log(noteValue);
-                console.log(notes[noteValue % notes.length]);
                 // play a note
-                synth.triggerAttackRelease(notes[noteValue % notes.length], '32n');
+                synth.triggerAttackRelease(feature.scaledTone, '32n');
             },
             feature.playtime * 1000,
-            i
         );
-        i++;
     }
 }
 
@@ -74,4 +82,16 @@ think about different notes
 think about effects
 display the earthquakes on the map
 map pitch to data
+
+
+2.5 or less 	Usually not felt, but can be recorded by seismograph. 	Millions
+2.5 to 5.4 	Often felt, but only causes minor damage. 	500,000
+5.5 to 6.0 	Slight damage to buildings and other structures. 	350
+6.1 to 6.9 	May cause a lot of damage in very populated areas. 	100
+7.0 to 7.9 	Major earthquake. Serious damage. 	10-15
+8.0 or greater 	Great earthquake. Can totally destroy communities near the epicenter. 	One every year or two
+
+
+
+take numbers between 0 and
 */
